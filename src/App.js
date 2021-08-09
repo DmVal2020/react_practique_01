2c607b7a5afad10700033127846318d3d6387397
DmVal2020 <blohala90@gmail.com>
2021-08-09 09:09:04 +0300

    создали файлы для главной страницы

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import Layout from './hoc/Layout/Layout';
+import Quiz from './containers/Quiz/Quiz';
 
 function App() {
   return (    
       <Layout>
-        <div style={{border:'2px solid grey',width:'200px',margin:'20px auto'}}>
-          <h1>First project</h1>
-        </div>        
+          <Quiz />
       </Layout> 
   );
 }
