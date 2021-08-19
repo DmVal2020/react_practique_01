50f4a121be9619f6a51282cbce491b8370c41f24
DmVal2020 <blohala90@gmail.com>
2021-08-19 11:11:45 +0300

    установили пакет react-router-dom и обернули проэкт в роутинг в файле index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const application = (
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+)
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {application}
   </React.StrictMode>,
   document.getElementById('root')
 );
