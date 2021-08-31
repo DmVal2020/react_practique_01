896bf8c6b3144862c3ed08c9a2ca39441609252f
DmVal2020 <blohala90@gmail.com>
2021-08-31 10:30:48 +0300

    инициализация инпутов формы

--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -39,6 +39,25 @@ export default class QuizCreator extends Component{
     }
     getQuestionHandler = ()=>{}
     creatTestHandler = ()=>{}
+    changeHandler = (event,controlName)=>{}
+    renderControls(){
+        return Object.keys(this.state.formControls).map((controlName,index)=>{
+            const control = this.state.formControls[controlName]
+            return(
+                <Input 
+                    key={index}
+                    label={control.label}
+                    value={control.value}
+                    valid={control.valid}
+                    touched={control.touched}
+                    chouldValidate={!!control.validation}
+                    erroMessage={control.errorMessage}
+                    onChange={event=>this.changeHandler(event.target.value,controlName)}
+                />
+
+            )
+        })
+    }
     render(){
 
         return(
@@ -46,12 +65,7 @@ export default class QuizCreator extends Component{
                 <div>
                     <h1>Создание теста</h1>
                     <form onSubmit={this.submitFormHandler}>
-                        <Input type="text"/>
-                        <hr/>
-                        <Input type="text"/>
-                        <Input type="text"/>
-                        <Input type="text"/>
-                        <Input type="text"/>
+                        {this.renderControls()}
 
                         <select></select>
 
