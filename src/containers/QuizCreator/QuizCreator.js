66bc6276bffd64166bfbed34afb260299af177a3
DmVal2020 <blohala90@gmail.com>
2021-08-31 09:29:26 +0300

    Создали в стате контроллы для инпутов формы, создали фремворк с методом для создания контролла, создали функцию которая автоматом заполняет похожие инпуты контроллом, вынесли в отдельный метод нулевое значение контролов стейта, для того чтобы при создании нового вопроса мы могли обнулять стейт

--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -1,9 +1,39 @@
 import React, {Component} from 'react'
 import classes from './QuizCreator.css'
+import {createControl} from '../../form/formFramework/formFramework'
 import Input from '../../components/FinishedQuiz/UI/Input/Input'
 import Button from '../../components/FinishedQuiz/UI/Button/Button'
 
+
+function createOptionControl(number){
+    return createControl(
+        {
+            label:`Вариант ${number}`,
+            errorMessage:"Значение не может быть пустым"
+        },
+        {required:true}
+    )
+}
+function createFormControls(){
+    return {
+            questions:createControl(
+                {
+                label:"введите вопрос",
+                errorMessage:"Поле вопроса не может быть пустым"
+                },
+                {required:true}
+            ),
+            option1:createOptionControl(1),
+            option2:createOptionControl(2),
+            option3:createOptionControl(3),
+            option4:createOptionControl(4),
+    }
+}
 export default class QuizCreator extends Component{
+    state={
+        quiz:[],
+        formControls:createFormControls(),
+    }
     submitFormHandler = event=>{
         event.preventDefault()
     }
