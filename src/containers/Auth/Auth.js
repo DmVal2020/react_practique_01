199c02181ded3669d749c7f5211316f8426e99b5
DmVal2020 <blohala90@gmail.com>
2021-09-05 09:50:58 +0300

    реализация авторизации и регистрации через firebase

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -3,6 +3,7 @@ import classes from './Auth.css'
 import Button from '../../components/FinishedQuiz/UI/Button/Button'
 import Input from '../../components/FinishedQuiz/UI/Input/Input'
 import is from 'is_js'
+import axios from 'axios'
 
 export default class Auth extends Component {
 
@@ -36,11 +37,33 @@ export default class Auth extends Component {
       }
     }
   
-    loginHandler = () => {
-  
+    loginHandler = async () => {
+      const authData = {
+        email:this.state.formControls.email.value,
+        password:this.state.formControls.password.value,
+        returnSecureToken:true
+      }
+      try{
+        const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBezgMR9XcFFLyDmZKbUo2ZVtBvWhvTy8E',authData)
+        console.log(response.data)
+      }catch(e){
+        console.log(e)
+      }
     }
   
-    registerHandler = () => {
+    registerHandler = async () => {
+      const authData = {
+        email:this.state.formControls.email.value,
+        password:this.state.formControls.password.value,
+        returnSecureToken:true
+      }
+      try{
+        const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBezgMR9XcFFLyDmZKbUo2ZVtBvWhvTy8E',authData)
+        console.log(response.data)
+      }catch(e){
+        console.log(e)
+      }
+      
   
     }
   
